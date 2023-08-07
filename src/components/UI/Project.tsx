662d309f1d5031afa0573ec8e1defe7bf9b66648
michiru6828 <since_l999@yahoo.co.jp>
2023-08-07 09:58:47 -0700

    fixed gif scale and project skills icon

--- src/components/UI/Project.tsx
+++ src/components/UI/Project.tsx
@@ -1,58 +1,18 @@
 import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { BsGithub } from 'react-icons/bs'
-import { AiOutlineLink } from 'react-icons/ai'
+import ProjectGifLink from './ProjectGifLink'
+import ProjectDetails from './ProjectDetails'
 
-function Project({ project }: { project: any }) {
+function Project({ project, isEven }: { project: any; isEven: boolean }) {
   return (
     <div
       className='transition-all ease-in duration-300 cursor-pointer top-0 pr-8 pl-8 
      mb-4 rounded-xl shadow-md border border-white bg-white/10'
     >
-      <div className='cursor-auto flex justify-between gap-10 py-3  transition-all duration-200 ease-in-out'>
-        <Link
-          href={project.url}
-          rel='noreferrer noopener'
-          target='_blank'
-          className='relative max-w-[100%] h-[300px] min-w-[500px] hover:scale-[1.3] transition-all duration-200 ease-in-out'
-        >
-          <Image
-            src={`/${project.src}.gif`}
-            alt={project.src}
-            layout='fill'
-            className='rounded-2xl '
-          />
-        </Link>
-        <div>
-          <div className='flex align-center font-decol gap-4'>
-            <h2 className='text-xl font-bold'>{project.title}</h2>
-            <div className='flex gap-2'>
-              <Link
-                href={project.repoUrl}
-                rel='noreferrer noopener'
-                target='_blank'
-                className='hover:opacity-50  hover:scale-[1.2] transition-all duration-200 ease-in-out '
-              >
-                <BsGithub size={25} />
-              </Link>
-              <Link
-                href={project.url}
-                rel='noreferrer noopener'
-                target='_blank'
-                className='hover:opacity-50  hover:scale-[1.2] transition-all duration-200 ease-in-out'
-              >
-                <AiOutlineLink size={25} />
-              </Link>
-            </div>
-          </div>
-          <div className='flex gap-2'>
-            {project.skills.map((skill: string, index: number) => {
-              return <p key={index}>{skill}</p>
-            })}
-          </div>
-          <p className='text-base '>{project.description}</p>
-        </div>
+      <div
+        className={`flex justify-between gap-20 py-4 cursor-auto  transition-all duration-200 ease-in-out`}
+      >
+        <ProjectGifLink project={project} />
+        <ProjectDetails project={project} />
       </div>
     </div>
   )
--- src/components/pages/home/sections/Projects.tsx
+++ src/components/pages/home/sections/Projects.tsx
@@ -7,8 +7,9 @@ function Projects() {
   return (
     <section id='projects' className='mt-[1rem]'>
       <h1 className='text-2xl font-bold font-decol'>プロジェクト</h1>
-      {projectsList.map((project: any) => {
-        return <Project key={project.title} project={project} />
+      {projectsList.map((project: any, index: number) => {
+        const isEven = index % 2 === 0
+        return <Project key={project.title} project={project} isEven={isEven} />
       })}
     </section>
   )
