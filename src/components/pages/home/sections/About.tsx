35bc347d468eadb9d3ba110254610a4a72eb60f7
michiru6828 <since_l999@yahoo.co.jp>
2023-08-22 15:41:21 -0700

    put margin

--- src/components/pages/home/sections/About.tsx
+++ src/components/pages/home/sections/About.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import TechSkills from '@/components/UI/TechSkills'
+import { aboutMe } from '@/const/aboutMe'
 
 function About() {
   return (
@@ -11,17 +12,7 @@ function About() {
       <div className='grow'>
         <h1 className='text-2xl font-decol font-bold mb-2'>自己紹介</h1>
 
-        <div className='text-base mb-5'>
-          　こんにちは！みちると申します。
-          1994年鹿児島県で生まれ幼少期を過ごしました。
-          高校時は3年間カナダへ単身留学し現地の公立高校を卒業、その後帰国し関西学院大学へ進学しました。
-          大学卒業後は、専門商社の人事採用担当として5年半勤務しました。
-          <br />
-          退職後は、キャリアチェンジを目指し再度カナダに渡り、2023年1月よりウェブ開発の専門学校に通っています。
-          現在は学校＋独学で開発スキルを日々磨いています。
-          <br />
-          もともとコンピュータが好きだったこともあり、苦戦しながらも開発を楽しんでいます。
-        </div>
+        <div className='text-base mb-11'>{aboutMe}</div>
 
         <div>
           <h1 className='text-xl font-bold font-decol mb-2'>スキル</h1>
