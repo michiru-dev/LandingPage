a1cb37daed75a6df8ff8111649c4447ad5c0a627
michiru6828 <since_l999@yahoo.co.jp>
2023-08-22 15:48:24 -0700

    fixed footer

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -9,11 +9,6 @@ import Footer from '@/components/UI/Footer'
 function Home() {
   return (
     <div>
-      {/* <div
-        className='fixed bg top--16 left-0 right-0 z-[-99] pointer-events-none 
-       flex justify-center items-center blur-[6rem] w-full h-[15rem] opacity-90 rotate-180 bg-[linear-gradient(90deg,#5700ff_0%,#7900ff_19%,#fd1d72_46%,#fd9931_73%,#fcb045_100%)]'
-      ></div> */}
-
       <section className='grid grid-cols-outer gap-y-36' id='home'>
         <div className='col-2-auto grid grid-cols-inner gap-12 grid-flow-dense'>
           <div className='col-start-1 col-end-13'>
