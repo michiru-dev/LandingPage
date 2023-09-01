2fe7830466eed05428e8cca1cb96cb2b51a195bd
michiru6828 <since_l999@yahoo.co.jp>
2023-09-01 16:00:08 -0700

    added responsive design

--- src/components/UI/WorkExperience.tsx
+++ src/components/UI/WorkExperience.tsx
@@ -1,11 +1,14 @@
 import { WorkExperienceType } from '@/types/workExperienceTypes'
-import React from 'react'
 import { hoverFlowClass } from './NavbarContents'
 
-const dotLineOuterDivClass =
-  'flex justify-center items-center absolute right-[30%] py-3'
+const dotLineOuterDivClass = 'flex justify-center items-center'
 const dotLineInnerDivClass =
   'h-[65px] border-dotted border-l-4 border-slate-300 mx-4 h-1'
+const outerDivClass =
+  'gap-2 min-[560px]:gap-20 text-sm min-[560px]:text-base flex flex-col min-[560px]:flex-row items-center'
+const innerDivClass = 'w-[100px] text-center'
+const nestedInnerDivClass =
+  'flex flex-col justify-center items-center w-[300px] min-[560px]:w-[350px] h-[80px] p-3'
 
 type WorkExperienceProps = {
   experience: WorkExperienceType
@@ -19,23 +22,28 @@ function WorkExperience({
   workExperience,
 }: WorkExperienceProps) {
   return (
-    <div className='relative h-[125px] w-fit'>
-      <div className='flex gap-10  items-center'>
-        <p className='font-decol w-[100px]'>{experience.date}</p>
+    <>
+      <div className={`${outerDivClass}`}>
+        <div className={`${innerDivClass} font-decol`}>{experience.date}</div>
         <div
-          className={` ${hoverFlowClass} flex flex-col justify-center items-center border border-solid border-slate-300 rounded-tl-[20px] rounded-br-[20px] rounded-tr-[2px] rounded-bl-[2px] w-[350px] h-[80px] p-3`}
+          className={` ${hoverFlowClass}  ${nestedInnerDivClass} border border-solid border-slate-300 rounded-tl-[20px] rounded-br-[20px] rounded-tr-[2px] rounded-bl-[2px]`}
         >
           <p> {experience.name}</p>
           <p>{experience.moreInfo}</p>
         </div>
       </div>
-
-      {index !== workExperience.length - 1 && (
-        <div className={dotLineOuterDivClass}>
-          <div className={dotLineInnerDivClass} />
+      {/* ↓dots div */}
+      <div className={`${outerDivClass}`}>
+        <div className={`${innerDivClass}`} />
+        <div className={`${nestedInnerDivClass}`}>
+          {index !== workExperience.length - 1 && (
+            <div className={dotLineOuterDivClass}>
+              <div className={dotLineInnerDivClass} />
+            </div>
+          )}
         </div>
-      )}
-    </div>
+      </div>
+    </>
   )
 }
 
